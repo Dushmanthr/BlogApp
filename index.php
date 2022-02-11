9c7cb9bf27d40a2283cbbe813494d227ffe4cf48
Dushmanthr <dushmantha15ranathunga@gmail.com>
2022-02-12 00:46:12 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+       include_once('inc/navbar.php');
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" 
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Russo+One&display=swap');
+</style>
+    <title>Blog App</title>
+    <link rel="stylesheet" href="css/index.css">
+
+</head>
+<body >
+
+   <div class="container">
+       <div class="row">
+           <div class="col-md-12">
+              <div class= "jumbotron mt-4">
+                  <h4 id = "jumbo-header">Welcome to the DonTube</h4>
+              </div>
+
+           </div>
+         
+       </div>
+   </div>
+
+    
+</body>
+
+
+</html>
