1617fcbe16584db9ca8fff57c46c2d78b9663bfc
Dushmanthr <dushmantha15ranathunga@gmail.com>
2022-02-12 21:40:15 +0530

    create signup form

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
@@ -27,10 +27,14 @@
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li> -->
       </ul>
-     <!--  <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form> -->
+    <ul class="navbar-nav >
+    <li class="nav-item style ="float:right; padding-left: 750px;">
+          <a class="nav-link " href="#" >Sign In</a> 
+          </li>
+
+          <li class="nav-item">
+          <a class="nav-link " href="signup.php" >Sign Up</a> </li>
+    </ul>
     </div>
   </div>
 </nav>
